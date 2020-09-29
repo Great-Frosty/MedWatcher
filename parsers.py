022d774f23afaee6799e3d5119874cac2f825ce1
Great Frosty <60777058+Great-Frosty@users.noreply.github.com>
2020-09-29 16:55:32 +0300

    Lancet parcer kinda works

--- parsers.py
+++ parsers.py
@@ -1,53 +1,65 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-from collections import deque
 import pandas as pd
 
 
-url = r'https://www.thelancet.com/online-first-research'
+def check_updates(existing, parse_limit=None):
+    '''Goes through the list of articles at a given url, parses and adds new
+    articles to storage.'''
 
-r = requests.get(url)
+    url = r'https://www.thelancet.com/online-first-research'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'lxml')
 
-soup = BeautifulSoup(r.text, 'lxml')
+    for article in soup.find_all('div', class_='article-details')[:parse_limit]:
+        text_url = article.find(href=re.compile(r'fulltext'))['href']
+        full_text_url = f'https://www.thelancet.com{text_url}'
 
-keywords = ['human', 'oocyte']
-relevant = []
-all_articles = pd.DataFrame(columns=['name', 'date published', 'url', 'contents'])
+        if full_text_url not in existing['url'].values:
+            data = parse_article(full_text_url)
+            existing = existing.append(data, ignore_index=True)
+        else:
+            print('We already have this article in storage')
 
-for article in soup.find_all('div', class_='article-details'):
-    text_url = article.find(href=re.compile(r'fulltext'))['href']
-    full_text_url = f'https://www.thelancet.com{text_url}'
+    return existing
 
 
-    if full_text_url not in all_articles['url']:
-        article_body = requests.get(full_text_url)
-        print(f'Opening {full_text_url}')
-        article_soup = BeautifulSoup(article_body.text, 'lxml')
+def parse_article(url):
+    '''Goes to a given article url, retrieves all the required info, returns it
+    as a pandas DataFrame.'''
 
-        article_name = article_soup.h1.text
-        article_date = article_soup.find('span', class_="article-header__publish-date__value").text
+    print(f'Parsing {url}')
+    article_body = requests.get(url)
+    article_soup = BeautifulSoup(article_body.text, 'lxml')
 
-        print(article_date)
+    article_name = article_soup.h1.text
+    article_date = article_soup.find('span', class_="article-header__publish-date__value").text
+    sections_text = [section.text for section in article_soup.find_all('div', class_='section-paragraph')]
+    article_text = '\n'.join(sections_text).split('References')[0]
+    article_contents = list(set([word.lower() for word in re.split(re.compile(r'\s'), article_text) if word.isalpha()]))
 
-#         all_articles['url'] = full_text_url
+    formatted_data = {
+        'name': article_name,
+        'date': article_date,
+        'url': url,
+        'contents': article_contents
+    }
 
-# print(all_articles['url'])
+    return formatted_data
 
-#     if full_text_url not in all_articles:
-#         all_articles.append(full_text_url)
 
-# print(*all_articles, sep='\n')
+with open('article_storage.json', 'r') as file:
 
-    # Заходим в каждой статье по кнопке Full-Text HTML, ищем ключевые слова
-    # article_body = requests.get(full_text_url)
-    # article_soup = BeautifulSoup(article_body.text, 'lxml')
+    try:
+        existing_articles = pd.read_json(file, orient='columns', convert_dates=False)
+        print('Storage successfully read!')
+    except ValueError:
+        print(f'Articles are empty')
+        columns = ['name', 'date', 'url', 'contents']
+        existing_articles = pd.DataFrame(columns=columns)
+        
+    existing_articles = check_updates(existing_articles)
 
-    # for section in article_soup.find_all('div', class_='section-paragraph'):
-    #     for keyword in keywords:
-    #         if keyword in section.text:
-    #             if full_text_url not in relevant:
-    #                 print(f'added an article\n{full_text_url}\nto relevant! Keyword = {keyword}')
-    #                 relevant.append(full_text_url)
-    #                 break
-
+with open('article_storage.json', 'w') as file:
+    existing_articles.to_json(file, orient='columns', date_format=None)
