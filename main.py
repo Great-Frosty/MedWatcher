cb9c7267a4cb2e7484fa546b11ae01b9ed7740f3
Great Frosty <60777058+Great-Frosty@users.noreply.github.com>
2020-10-13 19:53:10 +0300

    webhooks erased, config is live.

---
+++ main.py
@@ -0,0 +1,27 @@
+import logging
+import ssl
+import telebot
+import config
+from aiohttp import web
+
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
+bot = telebot.TeleBot(config.token)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message,
+                 ("Hi there, I am EchoBot.\n"
+                  "I am here to echo your kind words back to you."))
+
+
+# Handle all other messages
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
